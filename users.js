e58088309b3adb7e07f9174ac4448df9373e87f1
HFMIRE <62032608+HFMIRE@users.noreply.github.com>
2021-03-26 17:32:35 +0000

    commit

--- users.js
+++ users.js
@@ -3,14 +3,24 @@
 class User {
   constructor(name) {
     this.name = name;
+    this.electricScooter = [];
+    this.manualScooter = [];
   }
 }
 class ElectricScooter extends Scooter {
-  // this should hav settime function - takes 2s
-  //   setTimeout(() => {
-  //     alert(`Hi ${user}, Here is electric ${model}`);
-  // }, 2000)
+  userScooter(scooter) {
+    if (scooter instanceof ElectricScooter) {
+      this.electricScooter.push(scooter);
+      console.log("Here is your scooter");
+    } else {
+      console.log("Please wait for your scooter to charge");
+      setTimeout(() => {
+        alert(`Hi ${user}, Here is electric ${model}`);
+      }, 2000);
+    }
+  }
 }
+
 class ManualScooter extends Scooter {
   callManual() {
     console.log(`Hi ${user},Here is your Manual ${model}`);
